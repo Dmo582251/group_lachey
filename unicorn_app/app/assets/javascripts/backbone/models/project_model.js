d4a992d95977f00213b067fadcce6fd1171e5fb5
Dominique Mortelliti <dmortelliti0514@gmail.com>
2015-08-11 17:11:58 -0400

    my shat

--- unicorn_app/app/assets/javascripts/backbone/models/project_model.js
+++
@@ -1,6 +0,0 @@
-App.Models.Project = Backbone.Model.extend({
-	urlRoot: '/projects',
-	initialize: function(){
-		console.log("New Projects Model Created");
-	}
-});
--- unicorn_app/app/assets/javascripts/backbone/views/projects_view.js
+++ unicorn_app/app/assets/javascripts/backbone/views/projects_view.js
@@ -1,15 +1,1 @@
-App.Views.Projects = Backbone.View.extend({
-	el: '',
-	initialize: function(){
-		console.log("New Collection Project View Created");
-		this.listenTo(this.collection, 'reset', this.renderAllItems);
-		this.listenTo(this.collection, 'add', this.renderItem);
-	},
-	renderAllItems: function(){
-		this.collection.reversed().each(this.renderItem, this);
-	},
-	renderItem: function(model){
-		var newProjectView = new App.Views.Project({model : model});
-		this.$el.append(newProjectView.el);
-	}
-})
+
